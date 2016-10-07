22971f3ac36c8ae5ebc8e472f2a03f3938c0b766
raemilla <rachael.miller05@gmail.com>
2016-10-07 14:01:15 -0400

    create new story form without backend

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,11 @@
 class StoriesController < ApplicationController
+
+  def new
+    @words = ["pigs", "hats", "cats", "bowling balls", "princesses", "basketball", "baseball", "sweater vests", "Nickelback", "dev bootcamp", "New York City", "computers", "2048", "puppies", "carpets", "beards", "Bernie Sanders"]
+  end
+
+  def create
+    
+  end
+
 end
