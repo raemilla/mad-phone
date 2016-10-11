940e55b2438900bd866b34762ccbbf643bfc6c6e
mrudof <melissarudofsky@gmail.com>
2016-10-11 15:08:26 -0400

    added tabs

--- app/assets/javascripts/components/browse.es6.jsx
+++ app/assets/javascripts/components/browse.es6.jsx
@@ -8,7 +8,7 @@ class Browse extends React.Component {
     this.clickMostRecentlyCompleted= this.clickMostRecentlyCompleted.bind(this)
     this.clickMostRecentlyUpdated= this.clickMostRecentlyUpdated.bind(this)
   }
-  clickMostPopular() {
+  clickMostPopular(event) {
     this.setState({show: "mostPopular"})
   }
   clickMostRecentlyCompleted() {
@@ -21,18 +21,34 @@ class Browse extends React.Component {
 
   render () {
     let show
+    let MostPopularclasses
+    let MostRecentlyCompletedClasses
+    let MostRecentlyUpdatedClasses
     if (this.state.show === "mostPopular") {
        show = <MostPopular/>
+       MostPopularclasses = "nav-link active"
+       MostRecentlyCompletedClasses = "nav-link"
+       MostRecentlyUpdatedClasses = "nav-link"
      } else if (this.state.show === "mostRecentlyCompleted") {
        show = <MostRecentlyCompleted/>
+       MostPopularclasses = "nav-link"
+       MostRecentlyCompletedClasses = "nav-link active"
+       MostRecentlyUpdatedClasses = "nav-link"
      } else if (this.state.show === "mostRecentlyUpdated") {
        show = <MostRecentlyUpdated/>
+       MostPopularclasses = "nav-link"
+       MostRecentlyCompletedClasses = "nav-link"
+       MostRecentlyUpdatedClasses = "nav-link active"
      }
     return (
       <div>
-      <span onClick={this.clickMostPopular}>Top 10 </span>
-      <span onClick={this.clickMostRecentlyCompleted}>Recently Completed </span>
-      <span onClick={this.clickMostRecentlyUpdated}>Recently Updated </span>
+        <div className="container">
+          <ul className="nav nav-tabs">
+            <li className="nav-item" onClick={this.clickMostPopular}><a className={MostPopularclasses}>Top 10</a></li>
+            <li className="nav-item" onClick={this.clickMostRecentlyCompleted}><a className={MostRecentlyCompletedClasses}>Recently Completed </a></li>
+            <li className="nav-item" onClick={this.clickMostRecentlyUpdated}><a className={MostRecentlyUpdatedClasses}>Recently Updated </a></li>
+          </ul>
+        </div>
       <span>{show}</span>
       </div>
       )
