39ae78c10a2538110de1ded103db8b03930ba69f
raemilla <rachael.miller05@gmail.com>
2016-10-11 14:35:48 -0400

    voting functionality working but looks weird

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,23 @@
 class VotesController < ApplicationController
+  def new
+  end
+
+  def create
+    user = current_user
+    story = Story.find_by(id: params[:story])
+    story_sum = ""
+    if request.xhr?
+      # binding.pry
+      if params[:vote] == "upvote"
+        current_user.votes.create(value: 1, story_id: story.id)
+        # binding.pry
+        story_sum = "#{story.sum}"
+      elsif params[:vote] == "downvote"
+        current_user.votes.create(value: -1, story_id: story.id)
+        story_sum = "#{story.sum}"
+      end
+      render json: story_sum.to_json
+    end
+
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :user
+  belongs_to :story
+  
   validates :user_id, uniqueness: {scope: :story_id}
 
 end
