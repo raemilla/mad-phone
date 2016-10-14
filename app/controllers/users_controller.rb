2987276ffd0596bd0980fb2f163cd61091f3f1c8
psmathers <parkersmathers@yahoo.com>
2016-10-14 17:15:16 -0400

    add non-fb user image, hide buttons and notifications for non-current user show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
   before_action :require_user
   def show
+    # binding.pry
     @user = User.find_by(id: params[:id])
   end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.update_attributes(params[:description])
+  #     @user.save
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 end
