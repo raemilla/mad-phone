ac011e5c3ea70634a9f1b449396eff5b5cc94cbb
raemilla <rachael.miller05@gmail.com>
2016-10-14 14:41:01 -0400

    add description to user table with seed

--- db/migrate/20161007144340_create_users.rb
+++ db/migrate/20161007144340_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :name, null: false
       t.string :provider, null: false
+      t.text :description
       t.string :uid, null: false
       t.string :token, null: false
       t.timestamps(null: false)
