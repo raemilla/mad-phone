06292c55ae084fd3bd9d2d261a6a6e5ee6d807e7
raemilla <rachael.miller05@gmail.com>
2016-10-18 14:34:55 -0400

    refactor js interpolation to concatination

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
       url: "/votes",
       data: $(event.target).serialize()
     }).done(function(response){
-      $('.vote-total').replaceWith(`<span class='vote-total'>${response}</span>`);
+      $('.vote-total').replaceWith("<span class='vote-total'>"+response+"</span>");
     });
   });
 
@@ -27,7 +27,7 @@ $(document).ready(function(){
       url: "/comments",
       data: {comment: $('#new-comment-data').val(), story_id: $(event.target).children()[2].value}
     }).done(function(response){
-      $('.table').append(`<tr><td>${response.text}<br/><span class='additional-info'>posted by <a href='/users/${response.user_id}'>${response.user.name}</a> on ${response.created_at}</span></tr></td>`);
+      $('.table').append("<tr><td>"+response.text+"<br/><span class='additional-info'>posted by <a href='/users/"+response.user_id+"'>"+response.user.name+"</a> on " +response.created_at+" </span></tr></td>");
       $('#add-comment').children("#new-comment-data").val("");
     });
   });
