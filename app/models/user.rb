30d2cd2a0dc63401385264a1351bf5cd171051c6
raemilla <rachael.miller05@gmail.com>
2016-10-18 14:52:41 -0400

    fix contributed to user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :votes
   has_many :notifications
-  
+  has_many :contributed_stories, through: :sentences, source: :story
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.name = auth["info"]["name"]
