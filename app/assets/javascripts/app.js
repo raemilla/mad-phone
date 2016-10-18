2f8396adbbca9768a127e867560964e43f500110
raemilla <rachael.miller05@gmail.com>
2016-10-18 13:49:13 -0400

    change quotes around

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -27,9 +27,8 @@ $(document).ready(function(){
       url: "/comments",
       data: {comment: $('#new-comment-data').val(), story_id: $(event.target).children()[2].value}
     }).done(function(response){
-      console.log(response);
-      $('.table').append(`<tr><td>${response.text}<br/><span class="additional-info">posted by <a href="/users/${response.user_id}">${response.user.name}</a> on ${response.created_at}</span></tr></td>`)
-      $($('#add-comment').children()[3]).val("")
+      $('.table').append(`<tr><td>${response.text}<br/><span class='additional-info'>posted by <a href='/users/${response.user_id}'>${response.user.name}</a> on ${response.created_at}</span></tr></td>`)
+      $('#add-comment').children()[3]).val("")
     })
   });
 });
